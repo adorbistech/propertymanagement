322a414a33e48f660ff7aaab762f61b4c2bd2b39
Syed <adorbis.developer@gmail.com>
2024-12-23 05:05:19 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+  images: {
+    domains: ['example.com'], // Add any external image domains you're using
+  },
+}
+
+module.exports = nextConfig
+
